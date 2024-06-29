0792268e4ac4f66dafac8576889d55a15c48a1d4
Denys Horopashnyi <gdi1991@gmail.com>
2024-06-29 18:27:25 +0300

    Complete WDIO/Playwright - Home Task 1

---
+++ test/specs/test1.js
@@ -0,0 +1,33 @@
+import { expect, browser, $ } from '@wdio/globals';
+
+describe('Test 1 - Perform Login', () => {
+  it('Should log in to the "Swag Labs" page', async () => {
+    await browser.url(`https://www.saucedemo.com/`);
+    await $('#user-name').setValue('standard_user');
+    await $('#password').setValue('secret_sauce');
+    await $('#login-button').click();
+
+    const actualUrl = await browser.getUrl();
+    const expectedUrl = 'https://www.saucedemo.com/inventory.html';
+
+    expect(actualUrl).toBe(expectedUrl);
+  });
+
+  it("Should display 'Products' title", async () => {
+    const productsTitleText = await $('span[data-test="title"]').getText();
+
+    expect(productsTitleText).toBe('Products');
+  });
+
+  it('Should display Shopping Cart icon', async () => {
+    const shoppingCartIcon = await $('a[data-test="shopping-cart-link"]');
+
+    expect(shoppingCartIcon).toBeDisplayed();
+  });
+
+  it('Should display more than 1 product', async () => {
+    const shoppingItems = await $$('div[data-test="inventory-item"]');
+
+    expect(shoppingItems.length).toBeGreaterThan(1);
+  });
+});
